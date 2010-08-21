da1fd85f1a6fca9345442160d20bc42802539e9f
Nevon <tommy.brunn@gmail.com>
2010-08-22 00:13:35 +0200

    Replaced confusing UaLove with simple gamestate manager with proper documentation.

---
+++ intro.lua
@@ -0,0 +1,6 @@
+Gamestate.intro = Gamestate.new()
+local state = Gamestate.intro
+
+function state:enter()
+  print("Do shit!")
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
-require("ualove.init")
+require("gamestate")
+require("utils")
 
-hook.add("initial", function()
+require("intro")
+
+function love.load()
+  love.graphics.setBackgroundColor(50, 50, 50)
   love.graphics.setMode(800, 600, false, true, 0)
   love.graphics.setCaption("Fistful of Beef")
-  love.filesystem.setIdentity("love-party.fob")
-  love.graphics.setBackgroundColor(50, 50, 50)
-  love.filesystem.load("states/intro.lua")()
-  love.filesystem.load("states/menu.lua")()
-  game.state = "i-peace"
-end, "initial")
+  
+  images = {}
+  loadfromdir(images, "imgs", "png", love.graphics.newImage)
+  
+  Gamestate.registerEvents()
+	Gamestate.switch(Gamestate.intro)
+end
