a6788c878e3c3f07eef9ec02662d375f9555327d
unknown <Roybie@.(none)>
2010-08-22 01:11:58 +0100

    bleh

---
+++ main.lua
@@ -0,0 +1,30 @@
+require("gamestate")
+require("utils")
+require("soundmanager")
+
+--states requires
+require("states/intro")
+require("states/menu")
+require("states/game")
+
+--classes requires
+require("player")
+
+function love.load()
+  love.graphics.setBackgroundColor(50, 50, 50)
+  love.graphics.setMode(800, 600, false, true, 0)
+  love.graphics.setCaption("Fistful of Beef")
+  
+  images = {}
+  loadfromdir(images, "gfx", "png", love.graphics.newImage)
+  
+  sounds = {}
+  loadfromdir(sounds, "sfx", "ogg", love.sound.newSoundData)
+  
+  Gamestate.registerEvents()
+  local startstate = "intro"
+  if arg[2] then
+    startstate = arg[2]:match("--state=(.+)") or startstate
+  end
+  Gamestate.switch(Gamestate[startstate])
+end
