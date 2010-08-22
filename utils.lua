51c94c0dc203e6e64ea5c323c03e80367c14cb46
bartbes <bart.bes+projects@gmail.com>
2010-08-22 19:26:47 +0200

    Added bounding box collision function

--- utils.lua
+++ utils.lua
@@ -10,3 +10,18 @@ function loadfromdir(targettable, path, extension, func)
     end
   end
 end
+
+-- Collision detection function.
+-- Checks if box1 and box2 overlap.
+-- w and h mean width and height.
+function BoxBoxCollision(box1x, box1y, box1w, box1h, box2x, box2y, box2w, box2h)
+    if box1x > box2x + box2w - 1 or -- Is box1 on the right side of box2?
+       box1y > box2y + box2h - 1 or -- Is box1 under box2?
+       box2x > box1x + box1w - 1 or -- Is box2 on the right side of box1?
+       box2y > box1y + box1h - 1    -- Is b2 under b1?
+    then
+        return false                -- No collision. Yay!
+    else
+        return true                 -- Yes collision. Ouch!
+    end
+end
